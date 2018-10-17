7491afee48809af6cf8562b51ff361af887300dd
xiaotang999 <x.tang999@gmail.com>
2018-10-17 21:31:02 +0800

    x

--- apps/bbs/views.py
+++ apps/bbs/views.py
@@ -11,6 +11,8 @@ from .models import CommentSet
 class CommentSetView(View):
     def get(self, request):
         _messages = CommentSet.objects.order_by('-id')[:5]
+        # _num = CommentSet.objects.annotate(ount('id'))
+        # tag_list = CommentSet.objects.annotate(num_posts=Count('post'))
         _num = CommentSet.objects.annotate(Count('id'))
         return render(request, "zl955/test.html", {
             'messages':_messages,
