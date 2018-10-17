153de78f376472361dafd9c5cce55abf2b06f6bd
xiaotang999 <x.tang999@gmail.com>
2018-10-17 19:49:04 +0800

    xx

--- apps/bbs/views.py
+++ apps/bbs/views.py
@@ -2,14 +2,10 @@ import json
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 from .models import CommentSet
-
-
-
-
-
 
 class CommentSetView(View):
     def get(self, request):
