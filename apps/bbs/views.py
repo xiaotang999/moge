1370e728d6ebed5205c60e6d726f82ba29b0bc58
xiaotang999 <x.tang999@gmail.com>
2018-10-17 19:03:32 +0800

    xx

--- apps/bbs/views.py
+++ apps/bbs/views.py
@@ -12,46 +12,8 @@ class CommentSetView(View):
     """用户注册"""
     def get(self, request):
         _messages = CommentSet.objects.all()
+
         print(_messages)
-        return HttpResponse(_messages,content_type='application/json')
+        return HttpResponse(json.dumps(_messages),content_type='application/json')
     def post(self, request):
-        _messages = CommentSet.objects.all()
-        return HttpResponse(_messages,content_type='application/json')
-        
-        
- 
-
-
-# class LogoutView(View):
-#     """用户登出"""
-#     def get(self, request):
-#         logout(request)
-#         return HttpResponseRedirect(reverse("zl-index"))
-
-# class LoginView(View):
-#     """用户登录"""
-#     def get(self, request):
-#         if request.user.is_authenticated():
-#             return HttpResponseRedirect(reverse("zl-index"))
-#         else:
-#             return render(request, "zl955/login.html", {})
-#     def post(self, request):
-#         login_form = LoginForm(request.POST)
-#         if login_form.is_valid():
-#             user_name = request.POST.get("username", "")
-#             pass_word = request.POST.get("password", "")
-#             user = authenticate(username=user_name, password=pass_word)
-#             if user is not None:
-#                 if user.is_active:
-#                     login(request, user)
-#                     _status= {'msg':'登录成功','icon':'1'}
-#                     return HttpResponse(json.dumps(_status),content_type='application/json')
-#                 else:
-#                     _status= {'msg':'账户未激活，请联系管理员','icon':'5'}
-#                     return HttpResponse(json.dumps(_status),content_type='application/json')
-#             else:
-#                 _status= {'msg':'用户名或密码错误！','icon':'5'}
-#                 return HttpResponse(json.dumps(_status),content_type='application/json')
-#         else:
-#             _status= {'msg':'网络错误，请联系管理员','icon':'5'}
-#             return HttpResponse(json.dumps(_status),content_type='application/json')
+        pass
