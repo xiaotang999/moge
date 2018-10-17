caf4ba13bf28269a78e2daa83fbfd1d7f695aeef
xiaotang999 <x.tang999@gmail.com>
2018-10-17 22:56:58 +0800

    x

--- apps/bbs/views.py
+++ apps/bbs/views.py
@@ -10,6 +10,9 @@ from users.models import User
 
 class CommentSetView(View):
     def get(self, request):
+        _groups = 'null'
+        if request.user.is_authenticated():
+            _groups = User.objects.all()
         _messages = CommentSet.objects.order_by('-id')[:5]
         _people = User.objects.all().count()
         _num = CommentSet.objects.all().count()
@@ -17,4 +20,5 @@ class CommentSetView(View):
             'messages':_messages,
             'people' : _people,
             'num' : _num,
+            'groups' :_groups,
         })
