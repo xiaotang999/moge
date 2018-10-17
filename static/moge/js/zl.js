daad0169bd01a72c58c842a87a6d8f93e4a0dfa2
xiaotang999 <x.tang999@gmail.com>
2018-10-17 12:01:30 +0800

    xx

--- static/moge/js/zl.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("close1").onclick = function(){
-	document.getElementById("piaochuan1").style.display = "none"
-}
-document.getElementById("close2").onclick = function(){
-	document.getElementById("piaochuan2").style.display = "none"
-}
