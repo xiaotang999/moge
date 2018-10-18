4767e43285b4d75878a39deb2be8cc4f59e51998
xiaotang999 <x.tang999@gmail.com>
2018-10-18 14:53:48 +0800

    x

--- apps/zl955/views.py
+++ apps/zl955/views.py
@@ -54,8 +54,9 @@ class ZL955IndexView(View):
 		})
 	def post(self, request):
 		if request.user.is_authenticated():
+			print('asd')
 			received_json_data = json.loads(request.body)
-			return HttpResponse(json.dumps(request.body),content_type='application/json')
+			return HttpResponse(json.dumps(received_json_data),content_type='application/json')
 			# 'desc': data.field.desc,
 			# 'open_speak': userSet.open_speak,
 			# 'limit_speak_no': userSet.limit_speak_no,
