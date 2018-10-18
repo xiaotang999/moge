30dae5706ae27c8d1ebe0b24fc4a70f8e302f46d
xiaotang999 <x.tang999@gmail.com>
2018-10-18 16:09:09 +0800

    x

--- apps/zl955/views.py
+++ apps/zl955/views.py
@@ -60,9 +60,8 @@ class ZL955IndexView(View):
 			_changdu = len(_desc)
 			if request.user.group.open_speak:
 				if _changdu <= request.user.group.limit_speak_no:
-					_user = User()
-					_bbs = CommentSet()
-					_bbs.username = _user.username
+					_bbs = CommentSet.objects.filter(username=request.user.username)
+					# _bbs = CommentSet()
 					# _bbs.username = request.user.username
 					_bbs.desc = _desc
 					_bbs.save()
